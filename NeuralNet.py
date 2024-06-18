18730278c4924e28e20b3de87684c16563ea9d62
marlon moratti <moratti27@gmail.com>
2024-06-18 18:09:06 -0300

    adding bias term to neuralnet

--- NeuralNet.py
+++ NeuralNet.py
@@ -8,8 +8,8 @@ class NeuralNet(KeyClassifier):
     def __init__(self, state, hidden_layer_size=4):
         self.hidden_layer_size = hidden_layer_size
         fc1, fc2 = self._parse_state(state)
-        self.fc1 = lambda x: np.dot(fc1, x)
-        self.fc2 = lambda x: np.dot(fc2, x)
+        self.fc1 = lambda x: np.dot(np.concatenate(([1], x)), fc1)
+        self.fc2 = lambda x: np.dot(np.concatenate(([1], x)), fc2)
 
     def forward(self, x):
         x = self._relu(self.fc1(x))
@@ -23,8 +23,11 @@ class NeuralNet(KeyClassifier):
         return 'K_DOWN' if x < THRESHOLD else 'K_UP'
 
     def _parse_state(self, state):
-        n_input_weights = INPUT_LAYER_SIZE * self.hidden_layer_size
-        input_layer_weights = state[:n_input_weights].reshape(INPUT_LAYER_SIZE, self.hidden_layer_size)
+        input_layer_size = INPUT_LAYER_SIZE + 1     # adding bias term
+        assert len(state) == ((input_layer_size  * self.hidden_layer_size) + self.hidden_layer_size + 1)
+
+        n_input_weights = input_layer_size * self.hidden_layer_size
+        input_layer_weights = state[:n_input_weights].reshape(input_layer_size, self.hidden_layer_size)
         hidden_layer_weights = state[n_input_weights:]
         return input_layer_weights, hidden_layer_weights
 

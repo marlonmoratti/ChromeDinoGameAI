71e04038feb9768660363d6be7f6a6ec708d3ca7
marlon moratti <moratti27@gmail.com>
2024-06-18 01:34:54 -0300

    adding GA template

---
+++ GeneticAlgorithm.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class GeneticAlgorithm:
+    def __init__(self, fitness_fn):
+        self._fitness = fitness_fn
+
+    def evolve(self):
+        pass
+
+    def _initialize_population(self):
+        pass
+
+    def _parent_selection(self):
+        pass
+
+    def _cross_over(self):
+        pass
+
+    def _mutate(self):
+        pass
+
+    def _next_generation(self):
+        pass

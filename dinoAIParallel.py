70a8f57041113b9ea860723e318365c388d8125b
marlon moratti <moratti27@gmail.com>
2024-06-18 18:52:10 -0300

    modifying model and heuristics

--- dinoAIParallel.py
+++ dinoAIParallel.py
@@ -8,7 +8,7 @@ pygame.init()
 
 # Valid values: HUMAN_MODE or AI_MODE
 GAME_MODE = "AI_MODE"
-RENDER_GAME = True
+RENDER_GAME = False
 
 # Global Constants
 SCREEN_HEIGHT = 600
@@ -218,31 +218,6 @@ class KeyClassifier:
         pass
 
 
-def first(x):
-    return x[0]
-
-
-class KeySimplestClassifier(KeyClassifier):
-    def __init__(self, state):
-        self.state = state
-
-    def keySelector(self, distance, obHeight, speed, obType, nextObDistance, nextObHeight,nextObType):
-        self.state = sorted(self.state, key=first)
-        for s, d in self.state:
-            if speed < s:
-                limDist = d
-                break
-        if distance <= limDist:
-            if isinstance(obType, Bird) and obHeight > 50:
-                return "K_DOWN"
-            else:
-                return "K_UP"
-        return "K_NO"
-
-    def updateState(self, state):
-        self.state = state
-
-
 def playerKeySelector():
     userInputArray = pygame.key.get_pressed()
 
@@ -252,6 +227,9 @@ def playerKeySelector():
         return "K_DOWN"
     else:
         return "K_NO"
+
+
+from NeuralNet import NeuralNet
 
 
 def playGame(solutions):
@@ -278,7 +256,7 @@ def playGame(solutions):
 
     for solution in solutions:
         players.append(Dinosaur())
-        players_classifier.append(KeySimplestClassifier(solution))
+        players_classifier.append(NeuralNet(solution))
         solution_fitness.append(0)
         died.append(False)
 
@@ -385,61 +363,6 @@ def playGame(solutions):
     return solution_fitness
 
 
-# Change State Operator
-def change_state(state, position, vs, vd):
-    aux = state.copy()
-    s, d = state[position]
-    ns = s + vs
-    nd = d + vd
-    if ns < 15 or nd > 1000:
-        return []
-    return aux[:position] + [(ns, nd)] + aux[position + 1:]
-
-
-# Neighborhood
-def generate_neighborhood(state):
-    neighborhood = []
-    state_size = len(state)
-    for i in range(state_size):
-        ds = random.randint(1, 10)
-        dd = random.randint(1, 100)
-        new_states = [change_state(state, i, ds, 0), change_state(state, i, (-ds), 0), change_state(state, i, 0, dd),
-                      change_state(state, i, 0, (-dd))]
-        for s in new_states:
-            if s != []:
-                neighborhood.append(s)
-    return neighborhood
-
-
-# Gradiente Ascent
-def gradient_ascent(state, max_time):
-    start = time.process_time()
-    res, max_value = manyPlaysResultsTest(3, state)
-    better = True
-    end = 0
-    while better and end - start <= max_time:
-        neighborhood = generate_neighborhood(state)
-        better = False
-
-        results = playGame(neighborhood)
-        for i,value in enumerate(results):
-            if value > max_value:
-                state = neighborhood[i]
-                max_value = value
-                better = True
-        '''
-        for s in neighborhood:
-            aiPlayer = KeySimplestClassifier(s)
-            res, value = manyPlaysResults(3)
-            if value > max_value:
-                state = s
-                max_value = value
-                better = True
-        '''
-        end = time.process_time()
-    return state, max_value
-
-
 from scipy import stats
 import numpy as np
 
@@ -464,10 +387,17 @@ def manyPlaysResultsTest(rounds,best_solution):
     return (results, npResults.mean() - npResults.std())
 
 
+from GeneticAlgorithm import GeneticAlgorithm
+
+
 def main():
 
-    initial_state = [(15, 250), (18, 350), (20, 450), (1000, 550)]
-    best_state, best_value = gradient_ascent(initial_state, 5000)
+    ga = GeneticAlgorithm(
+        fitness_fn=lambda x: manyPlaysResultsTrain(1, x),
+        chromosome_length=37
+    )
+
+    best_state, best_value = ga.evolve(1000, 12*60)
     res, value = manyPlaysResultsTest(30, best_state)
     npRes = np.asarray(res)
     print(res, npRes.mean(), npRes.std(), value)
